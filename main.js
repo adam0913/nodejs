d3b36fa12dceb0446215f27a18c8f8011c4a346f
adam0913 <andykorea@gmail.com>
2023-04-29 10:11:01 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get('/sound/:name', (req, res) => {
+	const {name} = req.params
+	console.log(name)
+	if(name=='dog'){
+		res.json({'sound':'멍멍'})
+	}else if(name == 'cat'){
+		res.json({'sound':'야옹'})
+	}else{
+		res.send('알수없슴')
+	}
+	})
+
+app.listen(port, () => {})
